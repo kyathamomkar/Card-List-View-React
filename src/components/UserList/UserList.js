c7ae90a19e1e10f28d6fda2a29f1a7db1ec3facf
Omkar <40140860+kyathamomkar@users.noreply.github.com>
2021-12-06 09:38:59 -0600

    Refactoring fetching users
    using UseEffect for a lazy load

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,14 +1,33 @@
+import { useState, useEffect } from "react"
+import { Text } from "../DomElements"
 import { UserCard } from "./UserCard"
 import { createUser } from "../Utils/stringUtils"
-import userData from "../../Constants/users.json"
 import "./style.css"
 
 const UserList = () => {
+  const [userData, setUserData] = useState([])
+  const [errors, setErrors] = useState("")
+  useEffect(() => {
+    fetch("Constants/users.json")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length === 0) setErrors("No Users to Display")
+        setUserData((_) => data)
+      })
+      .catch((_) => {
+        setErrors("Error fetching the users")
+      })
+  }, [])
+
   return (
     <div className="userListContainer">
-      {userData.map((user) => (
-        <UserCard key={`${user.id}_${user.email}`} user={createUser(user)} />
-      ))}
+      {userData &&
+        userData.map((user) => (
+          <UserCard key={`${user.id}_${user.email}`} user={createUser(user)} />
+        ))}
+      <center>
+        <Text color="red">{errors}</Text>
+      </center>
     </div>
   )
 }
